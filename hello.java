c714725c85333a0167a738937910494230a16fae
KDaemon <33040126+KDaemon@users.noreply.github.com>
2017-10-23 17:04:51 -0400

    Add a feature
    Asks for a user name and outputs the users name and the  person greeting thems name as well

--- hello.java
+++ hello.java
@@ -1,13 +1,15 @@
 // a simple program to practice common Github commands 
-
+import java.util.Scanner;
 public class hello {
 
     public static void main(String [] args) {
 
-
+    	public String var = "Kevin";
 	// added  line	
-
-        System.out.println("Hello Class!");
+    	Scanner reader = new Scanner(System.in);  // Reading from System.in
+    	System.out.println("Enter a name: ");
+    	String n = reader.next(); // Scans the next token of the input as an int.
+        System.out.println("Hello " + n + ", my name is " + var);
 
 
 
